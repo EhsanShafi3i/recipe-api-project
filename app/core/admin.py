202b264e82f5c0a07cfef112ef9e0e05b2b8e807
Ehsan shafiei <mr.ehsanshafiei82@gmail.com>
2025-02-08 12:06:44 +0330

    feat(admin): Add user list field sets to admin page test(test_admin); Writing test for user list

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,40 @@
-# Register your models here.
+"""Django admin customization."""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define The admin pages for users."""
+
+    ordering = ("id",)
+    list_display = (
+        "email",
+        "name",
+    )
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        (("last login"), {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (None, {"fields": ("name", "email", "password1", "password2")}),
+        (
+            "Permissions",
+            {"fields": ("is_active", "is_staff", "is_superuser", "user_permissions")},
+        ),
+    )
+
+
+admin.site.register(models.User, UserAdmin)

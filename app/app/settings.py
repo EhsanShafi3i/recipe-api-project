8328c9d599a6b5ffd24c349e9314995da650af44
Ehsan shafiei <mr.ehsanshafiei82@gmail.com>
2025-02-08 13:44:46 +0330

    feat(urls.schema): Add swagger ui and schemas  to urls and settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # local Apps
     "core",
+    # Packages
+    "rest_framework",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +131,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "core.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}

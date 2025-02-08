aa0f00474f148fc6dd7ffab7f1a0829cda0cffb1
Ehsan shafiei <mr.ehsanshafiei82@gmail.com>
2025-02-09 00:16:36 +0330

    feat(api/user): add token creator and auth

--- app/user/views.py
+++ app/user/views.py
@@ -1,11 +1,20 @@
 """views for the user API."""
 
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from .serializers import UserSerializer
+from .serializers import AuthTokenSerializer, UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create Api for user creation."""
 
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user."""
+
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
